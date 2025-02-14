c3fb829e4756c77190f041aae4a4f52d521e852b
JuanINGSOFTWARE <juseroal@hotmail.com>
2025-02-14 01:23:03 +0000

    clase 2

---
+++ clase 2/Bucles.java
@@ -0,0 +1,12 @@
+public class Bucles {
+    public static void main (String[] args){
+        for (int i =1; i <=5; i++) {
+            System.out.println("Numero de iteracion: " + i);
+        }
+        int j=1;
+        while (j <= 5) {
+            System.out.println("numero de iteracion: " + j);
+            j++;
+        }
+    }
+}
---
+++ clase 2/variables.java
@@ -0,0 +1,14 @@
+public class variables
+{
+    //metodo main
+    public static void main(string[]args){
+        //tipos de datos Entero
+        int edad=25;
+        double altura = 1.75;
+        string nombre = "juan";
+        boolean esEstudiante = true; //false
+
+    }
+
+
+}
