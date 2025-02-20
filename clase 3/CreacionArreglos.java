72be40467e482f6f08a557865e8af2939a46163c
JuanINGSOFTWARE <juseroal@hotmail.com>
2025-02-20 23:55:07 +0000

    CreaciondeArreglos

---
+++ clase 3/CreacionArreglos.java
@@ -0,0 +1,16 @@
+public class CreacionArreglos {
+    public static void (String[]args){
+        //creamos el arreglo con numeros enteros
+        int[] a= { 3, 6, 5, 10, 20, 1, 4, 9};
+        // Recorremos y mostramos el elemento del arreglo 
+        //a.legth -> obtiene la longitud o tamaño del arreglo 
+        for(int i = 0; i a <a.legth; i++) {
+            System.out.println("a["+ 1 + "]=" + a[i])
+        }
+
+
+
+
+    }
+
+}
