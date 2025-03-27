acc5d11fd5d52648bba5bc0c933fceee1ff40cc3
JuanINGSOFTWARE <juseroal@hotmail.com>
2025-03-27 23:57:15 +0000

    creacion de la cuenta

---
+++ clase 8/cuenta.java
@@ -0,0 +1,17 @@
+public class cuenta{
+    private int numero;
+    private String tipo;
+    private double saldo;
+ 
+    public Cuenta(int numero, String tipo, double saldo) {
+        this.numero = numero;
+        this.tipo = tipo;
+        this.saldo = saldo;
+    }
+ 
+    public String toString() {
+        return "Cuenta { Nombre: " + numero +
+                " tipo: " + tipo +
+                " saldo: " + saldo + "}";
+    }
+}
